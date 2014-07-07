e5b29981b7d0bf3b725d0dccbedf2764511dff13
Michael Land <michael.z.land@gmail.com>
2014-07-07 12:16:04 -0700

    partial incorporation of LevelSwitcher module

---
+++ Assets/Scripts/LevelSwitcher/Editor/SwitchLevelsGUI.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(SwitchLevels))]
+
+public class SwitchLevelsGUI : Editor 
+{
+	//---------------------------
+	// Levels Toolbar variables
+	//---------------------------
+	//Keeps track of button changes
+	private int selectedLevel = 0; //TODO: Implement serialization, and start with the value that is saved.
+	private int toolbarSelected = 0; //TODO: Implement serialization, and start with the value that is saved.
+	private string[] toolbarLabels = new string[] {"Lv1", "Lv2", "Lv3", "Lv4", "Lv5"}; //Labels
+
+	//---------------------------
+	// Paint Options variables
+	//---------------------------
+	// booleans indicate checked boxes
+	private bool[] togglePaint = new bool[] {false, false, false, false, false, false, false, false, false, false};
+
+
+	public override void OnInspectorGUI()
+	{
+		//------------------
+		// Levels Toolbar
+		//------------------
+		GUILayout.Label ("Current Level:", EditorStyles.boldLabel);
+		toolbarSelected = GUILayout.Toolbar (toolbarSelected, toolbarLabels, GUILayout.Width(200) , GUILayout.Height(30));
+		//GUILayout.Button ("Apply", GUILayout.Width(70), GUILayout.Height(30));
+
+		//Tests which button is pressed
+		switch (toolbarSelected)
+		{
+			//Level 1
+			case 0:
+				if (toolbarSelected!=selectedLevel){
+					Debug.Log("Level 1 pressed");
+					changeLevel(1);
+				}
+				selectedLevel = toolbarSelected;
+			break;
+			//Level 2
+			case 1:
+				if (toolbarSelected!=selectedLevel){
+					Debug.Log("Level 2 pressed");
+					changeLevel(2);
+				}
+				selectedLevel = toolbarSelected;
+			break;
+			//Level 3
+			case 2:
+			if (toolbarSelected!=selectedLevel){
+					Debug.Log("Level 3 pressed");
+					changeLevel(3);
+				}
+				selectedLevel = toolbarSelected;
+			break;
+			//Level 4
+			case 3:
+				if (toolbarSelected!=selectedLevel){
+					Debug.Log("Level 4 pressed");
+					changeLevel(4);
+				}
+				selectedLevel = toolbarSelected;
+			break;
+			//Level 5
+			case 4:
+				if (toolbarSelected!=selectedLevel){
+					Debug.Log("Level 5 pressed");
+					changeLevel(5);
+				}
+				selectedLevel = toolbarSelected;
+			break;
+		}
+		
+		
+		
+		//------------------
+		// Paint Options
+		//------------------
+		GUILayout.Label ("Paint Options:", EditorStyles.boldLabel);
+		for(int i=0; i<10; i++){
+			GUILayout.BeginHorizontal("box", GUILayout.Width(200));
+				togglePaint[i] = GUILayout.Toggle(togglePaint[i], "Layer " + (i+1) );
+				GUILayout.Button ("Reset", GUILayout.Width(45), GUILayout.Height(15));
+			GUILayout.EndHorizontal();
+		}
+		GUILayout.BeginHorizontal (GUILayout.Width(200));
+			GUILayout.Button ("All ON", GUILayout.Width(97), GUILayout.Height(20));
+			GUILayout.Button ("All OFF", GUILayout.Width(97), GUILayout.Height(20));
+		GUILayout.EndHorizontal();
+
+		//------------------
+		// Eraser Options
+		//------------------
+		GUILayout.Label ("Eraser Options:", EditorStyles.boldLabel);
+		GUILayout.Space (-5);
+		GUILayout.Label ("This command subtracts the area\npainted with 'ColorX' from all visible\nPaint layers. ");
+
+		GUILayout.BeginHorizontal (GUILayout.Width(200));
+			GUILayout.Button ("Erase", GUILayout.Width(97), GUILayout.Height(20));
+			GUILayout.Button ("Reset Eraser", GUILayout.Width(97), GUILayout.Height(20));
+		GUILayout.EndHorizontal();
+		GUILayout.Space (15);
+
+	}
+
+	public void changeLevel(int levelId)
+	{
+		SwitchLevels myTarget = (SwitchLevels)target;
+		myTarget.switchLevel (levelId);
+	}
+
+}
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/LevelSwitcher/Scripts/SwitchLevels.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchLevels : MonoBehaviour {
+
+	//-----------------------
+	// Module Variables
+	//-----------------------
+	private int currentLevel = 0; //TODO: Implement serializable, so we start retrieving the saved level.
+
+	private bool initFlag = false;
+
+	// Terrains GameObjects
+	private GameObject terrainA;
+	private GameObject terrainB;
+	private GameObject terrainC;
+	private GameObject terrainD;
+
+	private TerrainData targetLevel;
+
+	public void switchLevel(int levelNum)
+	{
+		terrain1A = GameObject.Find("Terrain1A");
+		terrain1B = GameObject.Find("Terrain1B");
+		terrain1C = GameObject.Find("Terrain1C");
+		terrain1D = GameObject.Find("Terrain1D");
+
+		terrain2A = GameObject.Find("Terrain2A");
+		terrain2B = GameObject.Find("Terrain2B");
+		terrain2C = GameObject.Find("Terrain2C");
+		terrain2D = GameObject.Find("Terrain2D");
+
+		if (levelNum == 
+
+
+
+		switch (levelId)
+		{
+			case 1:
+				targetLevel = GameObject.Find("Level1").GetComponent<Terrain>().terrainData;
+			break;
+			
+			case 2:
+				targetLevel = GameObject.Find("Level2").GetComponent<Terrain>().terrainData;
+			break;
+
+			case 3:
+				targetLevel = GameObject.Find("Level3").GetComponent<Terrain>().terrainData;
+			break;
+			
+			case 4:
+				targetLevel = GameObject.Find("Level4").GetComponent<Terrain>().terrainData;
+			break;
+			
+			case 5:
+				targetLevel = GameObject.Find("Level5").GetComponent<Terrain>().terrainData;
+			break;
+
+			terrainA.GetComponent<Terrain>().terrainData = targetLevel;
+			terrainB.GetComponent<Terrain>().terrainData = targetLevel;
+			terrainC.GetComponent<Terrain>().terrainData = targetLevel;
+			terrainD.GetComponent<Terrain>().terrainData = targetLevel;
+		}
+	}
+}
