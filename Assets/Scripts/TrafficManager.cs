2fec4a4f60dc6529fc9776c2f2a7bd4f3d50b428
gabrielsina <gabrielsina@gmail.com>
2014-07-22 16:46:01 -0700

    Traffic manager sys. completed

--- Assets/Scripts/TrafficManager.cs
+++ Assets/Scripts/TrafficManager.cs
@@ -18,15 +18,15 @@ public class TrafficManager : MonoBehaviour {
 	private int newCarStartNode;
 	private int newCarEndNode;
 
-
-
 	// Update is called once per frame
 	void Update () {
 		// Road data
 		RoadInfo roadInfo;
+		RoadInfo partnerInfo;
 		// Amount of active cars in the road
 		int amountOfCars;
 		int numberOfNodes;
+		int partnerLength;
 		// Points to the game object
 		GameObject car;
 		// Points to the gamObject's component "CarInfo", which stores data about the car.
@@ -37,17 +37,24 @@ public class TrafficManager : MonoBehaviour {
 		{	
 			//Gets the "RoadInfo" component within the specific road we are looking at
 			roadInfo = roadsList[i].GetComponent<RoadInfo>();
+			partnerInfo = roadInfo.partnerRoad.GetComponent<RoadInfo>();
+
 			// Checks whether this road is enabled or not
 			if (roadInfo.enabled == false) break;
 
 			numberOfNodes = roadInfo.nodes.Length;
+			partnerLength = partnerInfo.nodes.Length;
 			
 			// See if we need to add a car to this road
 			if (Time.time >= roadInfo.nextCarCreationTime) {
-				GameObject newCar = createNewCar(roadInfo, numberOfNodes);
-				roadInfo.carList.Add(newCar);
-				//Debug.Log(roadInfo.carList);
-				roadInfo.nextCarCreationTime = Time.time + 60/roadInfo.averageCarsPerMinute;
+				GameObject newCar = createNewCar(roadInfo, numberOfNodes, partnerInfo, partnerLength);
+				// It will return NULL if the car should be created in the partner road,
+				// We don't want the road to be responsible for generating cars coming from the other direction
+				if(newCar != null)
+				{
+					roadInfo.carList.Add(newCar);
+					roadInfo.nextCarCreationTime = Time.time + (60/roadInfo.averageCarsPerMinute)+Random.Range(0, 5);
+				}
 			}
 			
 			// Iterate through each car on this road		
@@ -64,8 +71,9 @@ public class TrafficManager : MonoBehaviour {
 				// if percentTravelled > 100%
 				if(percentTravelled > 1.0f)
 				{
-					//Checks whether the next node the car is heading to exists or not.
+					//Checks whether the next node the car is heading to exists or not. (inside the same road)
 					int nextNode = carInfo.getNextNode();
+					// If car is has not reached any of the two extremes of the road
 					if(nextNode < numberOfNodes && nextNode != -1)
 					{
 						// Updates car path: sets segment from current position to next node's position
@@ -75,18 +83,16 @@ public class TrafficManager : MonoBehaviour {
 						carInfo.lookAtNextNode();
 					}
 
+					// Car has reached either node0 or nodeN 
 					else
 					{
-						// Gets the data about the partner road
-						RoadInfo partnerInfo = roadInfo.partnerRoad.GetComponent<RoadInfo>();
-						int partnerLength = partnerInfo.nodes.Length;
-
 						// We are going down (N to 0)
 						// And the car is at the border 0, now.
 						if(nextNode == -1)
 						{
 							// Computes distance between this border and the begining of next road
 							float nodesDistance = Vector3.Distance(car.transform.position, partnerInfo.nodes[partnerLength-1].position);
+							// If there is road to keep going ahead, we change the road the car is at
 							if(nodesDistance<50f)
 							{	
 								Transform newVirtualTargetNode = partnerInfo.nodes[partnerLength-1];
@@ -99,6 +105,7 @@ public class TrafficManager : MonoBehaviour {
 								// Add to the partner road
 								partnerInfo.carList.Add(car);
 							}
+							// We destroy the car, otherwise
 							else
 							{
 								int carId = j;
@@ -113,6 +120,7 @@ public class TrafficManager : MonoBehaviour {
 						{
 							// Computes distance between this border and the begining of next road
 							float nodesDistance = Vector3.Distance(car.transform.position, partnerInfo.nodes[0].position);
+							// If there is road to keep going ahead, we change the road the car is at
 							if(nodesDistance<50f)
 							{
 								Transform newVirtualTargetNode = partnerInfo.nodes[0];
@@ -125,6 +133,7 @@ public class TrafficManager : MonoBehaviour {
 								// Add to the partner road
 								partnerInfo.carList.Add(car);
 							}
+							// We destroy the car, otherwise
 							else
 							{
 								int carId = j;
@@ -160,11 +169,9 @@ public class TrafficManager : MonoBehaviour {
 		}
 	}
 
-	private GameObject createNewCar(RoadInfo roadInfo, int numberOfNodes)
+	private GameObject createNewCar(RoadInfo roadInfo, int numberOfNodes, RoadInfo partnerInfo, int partnerLength)
 	{
-		GameObject newCar = Instantiate(suvModel, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
-		CarInfo newCarInfo = newCar.GetComponent<CarInfo>();
-
+		bool carCreated = false;
 		// Getting information necessary to set car journey
 		float roadWidth = roadInfo.roadWidth;
 		int roadLanes = roadInfo.numLanes;
@@ -173,29 +180,57 @@ public class TrafficManager : MonoBehaviour {
 		// Gets max speed alowed by the lane this car will move at
 		float carMaxSpeed = roadInfo.laneSpeed[newCarLane-1];
 
+		//=============================================================
 		// Decides whether car is going "up" or "down" the road
 		// First half of the lanes go from node 0 to last node. (up)
 		// Second half of the lanes go from last node to 0. (down)
+		//=============================================================
+
+		// Decides which of the two roads is the begining of the big road (both together)
+		bool isThisFirstRoad = false;
+		float nodesDistance = Vector3.Distance(roadInfo.nodes[0].position, partnerInfo.nodes[partnerLength-1].position);
+		if(nodesDistance>100f) isThisFirstRoad = true;
+		// If car is in the first half of the lanes (going up, from 0 to N)
 		if(newCarLane <= roadLanes/2)
 		{
-			newCarFirstNode = roadInfo.nodes[0];
-			newCarSecondNode = roadInfo.nodes[1];
-			newCarStartNode = 0;
-			newCarEndNode = numberOfNodes-1;
+			// If this road is the first, the car will be generated at node 0 of this road
+			if (isThisFirstRoad == true)
+			{
+				newCarFirstNode = roadInfo.nodes[0];
+				newCarSecondNode = roadInfo.nodes[1];
+				newCarStartNode = 0;
+				newCarEndNode = numberOfNodes-1;
+				carCreated = true;
+			}
+			// We ignore if the car should come from the other direction (partner road)
 		}
+		// If car is in the first half of the lanes (going down, from N to 0)
 		else
 		{
-			newCarFirstNode = roadInfo.nodes[numberOfNodes-1];
-			newCarSecondNode = roadInfo.nodes[numberOfNodes-2];
-			newCarStartNode = numberOfNodes-1;
-			newCarEndNode = 0;
+			// If this road is the second, the car will be generated at the node N of this road
+			if (isThisFirstRoad == false)
+			{
+				newCarFirstNode = roadInfo.nodes[partnerLength-1];
+				newCarSecondNode = roadInfo.nodes[partnerLength-2];
+				newCarStartNode = partnerLength-1;
+				newCarEndNode = 0;
+				carCreated = true;
+			}
+			// We ignore if the car should come from the other direction (partner road)
 		}
 
-		// Sets the configuration above to the new car
-		newCarInfo.setJourney(newCarFirstNode, newCarSecondNode, newCarStartNode, newCarEndNode, roadWidth, roadLanes, newCarLane, carMaxSpeed);
-		
-		// returns a pointer to the new car GameObject
-		return newCar;
+		if(carCreated==true)
+		{
+			GameObject newCar = Instantiate(suvModel, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+			CarInfo newCarInfo = newCar.GetComponent<CarInfo>();
+
+			// Sets the configuration above to the new car
+			newCarInfo.setJourney(newCarFirstNode, newCarSecondNode, newCarStartNode, newCarEndNode, roadWidth, roadLanes, newCarLane, carMaxSpeed);
+			
+			// returns a pointer to the new car GameObject
+			return newCar;
+		}
+		return null;
 	}
 
 	private void destroyCar(CarInfo carInfo, RoadInfo roadInfo, int carId, GameObject car)
